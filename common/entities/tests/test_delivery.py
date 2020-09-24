e39f7407f38b4cc0d9573df872a471a4cc3201cc
Roee Ben Sushan <roeebs@visionmap.com>
2020-09-24 15:55:46 +0300

    Refactoring after code review

--- common/entities/tests/test_delivery.py
+++ common/entities/tests/test_delivery.py
@@ -1,27 +1,28 @@
 import unittest
 
-from common.entities.package_factory import package_factory, package_delivery_plan_factory
+from common.entities.package import PackageType
+from common.entities.package_factory import package_delivery_plan_factory
+from common.math.angle import Angle
+from geometry.geo_factory import create_point_2d
 
 
 class BasicPackageGeneration(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.p1 = package_factory("Tiny")
-        cls.p2 = package_factory("Small")
-        cls.p3 = package_factory("Medium")
-        cls.p4 = package_factory("Large")
-        cls.p5 = package_factory("mini")
-        cls.pdp = package_delivery_plan_factory(x=0, y=0, arrival_angle=0.1, hitting_angle=1.81, package_type="Tiny")
+        cls.p1 = PackageType.TINY
+        cls.p2 = PackageType.SMALL
+        cls.p3 = PackageType.MEDIUM
+        cls.p4 = PackageType.LARGE
+        point = create_point_2d(1,2)
+        cls.pdp = package_delivery_plan_factory(point, azimuth=Angle(30), elevation=Angle(80),
+                                                package=PackageType.TINY)
 
     def test_package_type(self):
-        self.assertEqual(self.p1.type(), "Tiny")
-        self.assertEqual(self.p2.type(), "Small")
-        self.assertEqual(self.p3.type(), "Medium")
-        self.assertEqual(self.p4.type(), "Large")
-        self.assertEqual(self.p5, None)
+        self.assertEqual(self.p1.value.size, 1)
+        self.assertEqual(self.p2.value.size, 2)
+        self.assertEqual(self.p3.value.size, 4)
+        self.assertEqual(self.p4.value.size, 8)
 
     def test_package_delivery_plan(self):
-        self.assertEqual(self.pdp.package.type(), "Tiny")
-
-
+        self.assertEqual(self.pdp.package.value.size, 1)
