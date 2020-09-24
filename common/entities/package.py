231230bfb59d8c19c223599128afd6ed78abebc9
Roee Ben Sushan <roeebs@visionmap.com>
2020-09-24 15:55:57 +0300

    Refactoring after code review

--- common/entities/package.py
+++ common/entities/package.py
@@ -1,84 +1,20 @@
 from __future__ import annotations
 
-from abc import ABC
-import copy
+from enum import Enum
 
 
-class Package(ABC):
+class _Package:
 
-    @property
-    def type(self) -> str:
-        raise NotImplementedError()
+    def __init__(self, size: float):
+        self._size = size
 
     @property
-    def weight_kg(self) -> float:
-        raise NotImplementedError()
-
-    def clone(self):
-        pass
+    def size(self):
+        return self._size
 
 
-class _TinyPackage(Package):
-
-    def __init__(self):
-        self._type = "Tiny"
-        self._weight_kg = 1
-
-    def type(self) -> str:
-        return self._type
-
-    def weight_kg(self) -> float:
-        return self._weight_kg
-
-    def clone(self):
-        return copy.copy(self)
-
-
-class _SmallPackage(Package):
-
-    def __init__(self):
-        self._type = "Small"
-        self._weight_kg = 2
-
-    def type(self) -> str:
-        return self._type
-
-    def weight_kg(self) -> float:
-        return self._weight_kg
-
-    def clone(self):
-        return copy.copy(self)
-
-
-class _MediumPackage(Package):
-
-    def __init__(self):
-        self._type = "Medium"
-        self._weight_kg = 4
-
-    def type(self) -> str:
-        return self._type
-
-    def weight_kg(self) -> float:
-        return self._weight_kg
-
-    def clone(self):
-        return copy.copy(self)
-
-
-class _LargePackage(Package):
-
-    def __init__(self):
-        self._type = "Large"
-        self._weight_kg = 8
-
-    def type(self) -> str:
-        return self._type
-
-    def weight_kg(self) -> float:
-        return self._weight_kg
-
-    def clone(self):
-        return copy.copy(self)
-
-
+class PackageType(Enum):
+    TINY = _Package(1)
+    SMALL = _Package(2)
+    MEDIUM = _Package(4)
+    LARGE = _Package(8)
