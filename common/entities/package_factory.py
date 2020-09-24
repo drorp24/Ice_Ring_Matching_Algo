01f8deba30a94ef6672e545da06c6fad7c2108f8
Roee Ben Sushan <roeebs@visionmap.com>
2020-09-24 15:56:17 +0300

    Refactoring after code review

--- common/entities/package_factory.py
+++ common/entities/package_factory.py
@@ -1,19 +1,10 @@
 from __future__ import annotations
 
-from common.entities.package import Package, _TinyPackage, _SmallPackage, _MediumPackage, _LargePackage
-from common.entities.package_delivery_plan import PackageDeliveryPlan
-
-__package_dict = {"Tiny": _TinyPackage(), "Small": _SmallPackage(),
-                  "Medium": _MediumPackage(), "Large": _LargePackage()}
+from common.entities.package import _Package, PackageType
+from common.entities.package_delivery_plan import Angle,  PackageDeliveryPlan
+from geometry.geo2d import Point2D
 
 
-def package_factory(package_type: str) -> Package:
-    if __package_dict.get(package_type) is not None:
-        return __package_dict[package_type].clone()
-    return None
-
-
-def package_delivery_plan_factory(x: float, y: float, arrival_angle: float, hitting_angle: float,
-                                  package_type: str) -> PackageDeliveryPlan:
-    package = package_factory(package_type)
-    return PackageDeliveryPlan(x, y, arrival_angle, hitting_angle, package)
+def package_delivery_plan_factory(point: Point2D, azimuth: Angle, elevation: Angle,
+                                  package: _Package) -> PackageDeliveryPlan:
+    return PackageDeliveryPlan(point, azimuth, elevation, package)
