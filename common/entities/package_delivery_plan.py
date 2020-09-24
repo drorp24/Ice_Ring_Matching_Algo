a27a9ee8c05db4b2f99cee6e81f48773bc824a3f
Roee Ben Sushan <roeebs@visionmap.com>
2020-09-24 15:56:07 +0300

    Refactoring after code review

--- common/entities/package_delivery_plan.py
+++ common/entities/package_delivery_plan.py
@@ -1,25 +1,12 @@
-from common.entities.package import Package
-from geometry.geo2d import Point2D, Vector2D
-from geometry.geo_factory import create_point_2d
+from common.entities.package import PackageType
+from geometry.geo2d import Point2D
+from common.math.angle import Angle
 
 
 class _DropPoint:
 
-    def __init__(self, x: float, y: float):
-        self._coordinates = create_point_2d(x, y)
-
-    @property
-    def coordinates(self) -> Point2D:
-        return self._coordinates
-
-    def set_x(self, x: float):
-        self._coordinates.x = x
-
-    def set_y(self, y: float):
-        self._coordinates.y = y
-
-    def shift(self, v: Vector2D):
-        self._coordinates = self._coordinates.add_vector(v)
+    def __init__(self, point: Point2D):
+        self._coordinates = point
 
     def __str__(self):
         return "({},{})".format(self._coordinates.x, self._coordinates.y)
@@ -27,10 +14,10 @@ class _DropPoint:
 
 class PackageDeliveryPlan:
 
-    def __init__(self, x: float, y: float, arrival_angle: float, hitting_angle: float, package: Package):
-        self._drop_point = _DropPoint(x, y)
-        self._arrival_angle = arrival_angle
-        self._hitting_angle = hitting_angle
+    def __init__(self, point: Point2D, azimuth: Angle, elevation: Angle, package: PackageType):
+        self._drop_point = _DropPoint(point)
+        self._azimuth = azimuth
+        self._elevation = elevation
         self._package = package
 
     @property
@@ -38,19 +25,13 @@ class PackageDeliveryPlan:
         return self._drop_point
 
     @property
-    def arrival_angle(self) -> float:
-        return self._arrival_angle
+    def azimuth_deg(self) -> Angle:
+        return self._azimuth
 
     @property
-    def hitting_angle(self) -> float:
-        return self._hitting_angle
+    def elevation_deg(self) -> Angle:
+        return self._elevation
 
     @property
-    def package(self) -> Package:
+    def package(self) -> PackageType:
         return self._package
-
-    def get_package_type(self) -> str:
-        return self._package.type()
-
-    def get_package_weight(self) -> float:
-        return self._package.weight_kg()
