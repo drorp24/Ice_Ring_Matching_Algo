b869580a8a811b73fc0cc973004feb71374a739c
Ronen Ben-Hayon <ronenbh1@gmail.com>
2020-09-29 18:20:59 +0300

    Fixed bug in delivery tests

--- common/entities/tests/test_delivery.py
+++ common/entities/tests/test_delivery.py
@@ -27,13 +27,13 @@ class BasicDeliveryGeneration(unittest.TestCase):
                                          time_window=cls.time_window, priority=10)
 
     def test_customer_delivery(self):
-        self.assertEqual(self.cd.package_delivery_plans[0].package.value.size, 1)
+        self.assertEqual(self.cd.package_delivery_plans[0].package.type.value, 1)
 
     def test_delivery_option(self):
-        self.assertEqual(self.do.customer_deliveries[0].package_delivery_plans[0].package.value.size, 1)
+        self.assertEqual(self.do.customer_deliveries[0].package_delivery_plans[0].package.type.value, 1)
 
     def test_delivery_request(self):
         self.assertFalse(self.dr.time_window.since > self.dr.time_window.until)
         self.assertEqual(self.dr.priority, 10)
         self.assertEqual(
-            self.dr.delivery_options[0].customer_deliveries[0].package_delivery_plans[0].package.value.size, 1)
+            self.dr.delivery_options[0].customer_deliveries[0].package_delivery_plans[0].package.type.value, 1)
