374bfe1494e8de633901793472a8161744d8ea0e
Gil Elbaz <gil.elbaz13@gmail.com>
2020-10-01 12:16:03 +0000

    * fixing problems with empty polygon * make intersection work * testing polygon

--- geometry/geo_factory.py
+++ geometry/geo_factory.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
-from geometry.geo2d import Point2D, Vector2D
+from typing import List
+
+from geometry.geo2d import Point2D, Vector2D, MultiPolygon2D
+from geometry.geo2d import Polygon2D, LineString2D, LinearRing2D
 from geometry.math_wrapper import _MathVector2D
-from geometry.shapely_wrapper import _ShapelyPoint2D
+from geometry.shapely_wrapper import _ShapelyPoint2D, _ShapelyMultiPolygon2D
+from geometry.shapely_wrapper import _ShapelyPolygon2D, _ShapelyLineString2D, _ShapelyLinearRing2D
 
 
 def create_point_2d(x: float, y: float) -> Point2D:
@@ -19,3 +23,19 @@ def create_vector_2d(x: float, y: float) -> Vector2D:
 
 def convert_to_vector(point: Point2D) -> Vector2D:
     return create_vector_2d(point.x, point.y)
+
+
+def create_polygon_2d(points: List[Point2D]) -> Polygon2D:
+    return _ShapelyPolygon2D(points)
+
+
+def create_multipolygon_2d(polygons: List[Polygon2D]) -> MultiPolygon2D:
+    return _ShapelyMultiPolygon2D(polygons)
+
+
+def create_line_string_2d(points: List[Point2D]) -> LineString2D:
+    return _ShapelyLineString2D(points)
+
+
+def create_linear_ring_2d(points: List[Point2D]) -> LinearRing2D:
+    return _ShapelyLinearRing2D(points)
