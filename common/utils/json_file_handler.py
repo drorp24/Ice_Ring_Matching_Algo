4618d4d26920f9b4ac0505a43eec8e86140ed0fe
GUYKL <guykl@rafael.co.il>
2020-10-06 10:58:26 +0300

    Add type hints Replace delivery_request_conf.py with json_file_handler.py

---
+++ common/utils/json_file_handler.py
@@ -0,0 +1,24 @@
+import json
+import platform
+from pathlib import Path, PureWindowsPath
+
+
+def __get_path(file_path):
+    system = platform.system()
+    filename = Path(file_path)
+    if system is 'windows':
+        filename = PureWindowsPath(filename)
+    return Path(filename)
+
+
+def from_file(file_path) -> 'dict':
+    correct_path = __get_path(file_path)
+    with open(correct_path, "r") as file:
+        return json.load(file)
+
+
+def to_file(file_path, data):
+    correct_path = __get_path(file_path)
+
+    with open(correct_path, 'w') as file:
+        json.dump(data, file, indent=2)
