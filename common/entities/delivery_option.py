07783057f0aa36acc13ee2dadde9247777237210
Roee Ben Sushan <roeebs@visionmap.com>
2020-10-11 16:35:17 +0300

    refactoring after code review

--- common/entities/delivery_option.py
+++ common/entities/delivery_option.py
@@ -4,9 +4,6 @@ from common.entities.customer_delivery import CustomerDelivery
 class DeliveryOption:
 
     def __init__(self, customer_deliveries: [CustomerDelivery]):
-        if not isinstance(customer_deliveries, list):
-            raise TypeError("customer_deliveries must be a list")
-
         self._customer_deliveries = customer_deliveries if customer_deliveries is not None else []
 
     @property
--- common/entities/package_delivery_plan.py
+++ common/entities/package_delivery_plan.py
@@ -3,25 +3,16 @@ from geometry.geo2d import Point2D
 from common.math.angle import Angle
 
 
-class DropPoint:
-
-    def __init__(self, point: Point2D):
-        self._coordinates = point
-
-    def __str__(self):
-        return "({},{})".format(self._coordinates.x, self._coordinates.y)
-
-
 class PackageDeliveryPlan:
 
-    def __init__(self, point: Point2D, azimuth: Angle, elevation: Angle, package: PackageType):
-        self._drop_point = DropPoint(point)
+    def __init__(self, drop_point: Point2D, azimuth: Angle, elevation: Angle, package: PackageType):
+        self._drop_point = drop_point
         self._azimuth = azimuth
         self._elevation = elevation
         self._package = package
 
     @property
-    def drop_point(self) -> DropPoint:
+    def drop_point(self) -> Point2D:
         return self._drop_point
 
     @property
