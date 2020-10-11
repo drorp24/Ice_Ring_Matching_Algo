84f8689d542ef6635754e9431dae134b3f7b66f9
Roee Ben Sushan <roeebs@visionmap.com>
2020-10-11 17:03:48 +0300

    refactoring after code review

--- common/utils/json_file_handler.py
+++ common/utils/json_file_handler.py
@@ -3,21 +3,20 @@ import platform
 from pathlib import Path, PureWindowsPath
 
 
-def __get_path(file_path: str) -> str:
+def __get_path(file_path: Path) -> Path:
     system = platform.system()
-    filename = Path(file_path)
     if system is 'windows':
-        filename = PureWindowsPath(filename)
-    return Path(filename)
+        file_path = PureWindowsPath(file_path)
+    return file_path
 
 
-def from_file(file_path: str) -> dict:
+def from_file(file_path: Path) -> dict:
     correct_path = __get_path(file_path)
     with open(correct_path, "r") as file:
         return json.load(file)
 
 
-def to_file(file_path: str, data: dict) -> None:
+def to_file(file_path: Path, data: dict) -> None:
     correct_path = __get_path(file_path)
 
     with open(correct_path, 'w') as file:
