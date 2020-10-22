f05647d843a5bc459aa8c98c6ed504bf58d49a39
gilbaz <gil.elbaz13@gmail.com>
2020-10-22 12:30:11 +0300

    initial wrapping of graph

--- common/graph/operational/delivery_request_graph.py
+++ common/graph/operational/delivery_request_graph.py
@@ -7,19 +7,19 @@ from common.graph.base_graph.graph_wrapper import *
 class DeliveryRequestNode(GraphNode):
 
     def __init__(self, delivery_request: DeliveryRequest):
-        super.__init__(node=delivery_request)
+        super().__init__(node=delivery_request)
 
 
 class DeliveryPath(DirectedEdge):
 
     def __init__(self, start_request: DeliveryRequest, end_request: DeliveryRequest):
-        super.__init__(start_node=start_request, end_node=end_request)
+        super().__init__(start_node=DeliveryRequestNode(start_request), end_node=DeliveryRequestNode(end_request))
 
 
 class DeliveryRequestGraph(DirectedGraph):
 
     def __init__(self):
-        super.__init__()
+        super().__init__()
 
     def add_delivery_requests(self, delivery_requests: [DeliveryRequest]):
         self.add_nodes(delivery_requests)
