c384c5dded465b4ee2af6dc5b3033619d881f954
gilbaz <gil.elbaz13@gmail.com>
2020-11-04 16:34:04 +0200

    creating distributions!

--- common/entities/tests/test_package_delivery_plan.py
+++ common/entities/tests/test_package_delivery_plan.py
@@ -14,5 +14,13 @@ class BasicPackageDeliveryPlan(unittest.TestCase):
         num_instances = 10
         samples_from_distribution = self.package_delivery_distrib.choose_rand(Random(), num_instances)
         self.assertEqual(len(samples_from_distribution), num_instances)
-        unique_samples = len(set(samples_from_distribution))
+        unique_samples = len(set(samples_from_distribution * 5))
         self.assertEqual(unique_samples, num_instances)
+
+    def test_random_generation_is_reproducable(self):
+        samples_from_distribution1 = self.package_delivery_distrib.choose_rand(Random(100), 100)
+        samples_from_distribution2 = self.package_delivery_distrib.choose_rand(Random(100), 100)
+        self.assertEqual(samples_from_distribution1, samples_from_distribution2)
+
+        samples_from_distribution3 = self.package_delivery_distrib.choose_rand(Random(103), 100)
+        self.assertNotEqual(samples_from_distribution1, samples_from_distribution3)
