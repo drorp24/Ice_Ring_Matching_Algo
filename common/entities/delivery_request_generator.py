1bcee625dcb7e36ea5ba3ffb4263218c1d900d5d
gilbaz <gil.elbaz13@gmail.com>
2020-11-11 15:41:43 +0200

    improved pr w roey

--- common/entities/delivery_request_generator.py
+++ common/entities/delivery_request_generator.py
@@ -9,10 +9,10 @@ from common.entities.delivery_request import DeliveryRequestDistribution, Delive
 
 @dataclass
 class DeliveryRequestDatasetStructure:
-    num_of_delivery_requests: Union[int, Range] = 2
-    num_of_delivery_options_per_delivery_request: Union[int, Range] = 5
-    num_of_customer_deliveries_per_delivery_option: Union[int, Range] = 10
-    num_of_package_delivery_plan_per_customer_delivery: Union[int, Range] = 3
+    num_of_delivery_requests: int = 2
+    num_of_delivery_options_per_delivery_request: int = 5
+    num_of_customer_deliveries_per_delivery_option: int = 10
+    num_of_package_delivery_plan_per_customer_delivery: int = 3
     delivery_request_distribution: DeliveryRequestDistribution = DeliveryRequestDistribution()
 
 
