08acafa51a52d7dd55c56cae8bcde2ecc8a979cb
meital issers <meitalg@gmail.com>
2020-11-25 15:49:27 +0200

    split init to functions

--- grid/slides_container.py
+++ grid/slides_container.py
@@ -22,25 +22,18 @@ class SlidesContainer:
     def __init__(self, drone_azimuth_resolution: int, drop_azimuth_resolution: int,
                  cell_width_resolution: float, cell_height_resolution: float, slides: List[Slide]):
 
-        if drone_azimuth_resolution <= 0:
-            raise ValueError("Drone azimuth resolution should be more than 0")
-
-        if drop_azimuth_resolution <= 0:
-            raise ValueError("Drop azimuth resolution should be more than 0")
+        self.is_drone_az_resolution_positive(drone_azimuth_resolution)
+        self.is_drop_az_resolution_positive(drop_azimuth_resolution)
 
         self._drone_azimuth_resolution = drone_azimuth_resolution
         self._drone_azimuth_delta_deg = Angle(MAX_AZIMUTH_DEGREES / self._drone_azimuth_resolution,
                                               AngleUnit.DEGREE)
-
-        if not self._drone_azimuth_delta_deg.degrees.is_integer():
-            raise ValueError(MAX_AZIMUTH_DEGREES, "should be divided by drone azimuth resolution with no remainder")
+        self.is_drone_azimuth_delta_deg_integer()
 
         self._drop_azimuth_resolution = drop_azimuth_resolution
         self._drop_azimuth_delta_deg = Angle(MAX_AZIMUTH_DEGREES / self._drop_azimuth_resolution,
                                              AngleUnit.DEGREE)
-
-        if not self._drop_azimuth_delta_deg.degrees.is_integer():
-            raise ValueError(MAX_AZIMUTH_DEGREES, "should be divided by drop azimuth resolution with no remainder")
+        self.is_drop_azimuth_delta_deg_integer()
 
         self._cell_width_resolution = cell_width_resolution
         self._cell_height_resolution = cell_height_resolution
@@ -49,6 +42,22 @@ class SlidesContainer:
         for slide in slides:
             self._hash[slide.package_type][slide.drone_azimuth.degrees][
                 slide.drop_azimuth.degrees] = slide
+
+    def is_drop_azimuth_delta_deg_integer(self):
+        if not self._drop_azimuth_delta_deg.degrees.is_integer():
+            raise ValueError(MAX_AZIMUTH_DEGREES, "should be divided by drop azimuth resolution with no remainder")
+
+    def is_drone_azimuth_delta_deg_integer(self):
+        if not self._drone_azimuth_delta_deg.degrees.is_integer():
+            raise ValueError(MAX_AZIMUTH_DEGREES, "should be divided by drone azimuth resolution with no remainder")
+
+    def is_drop_az_resolution_positive(self, drop_azimuth_resolution):
+        if drop_azimuth_resolution <= 0:
+            raise ValueError("Drop azimuth resolution should be more than 0")
+
+    def is_drone_az_resolution_positive(self, drone_azimuth_resolution):
+        if drone_azimuth_resolution <= 0:
+            raise ValueError("Drone azimuth resolution should be more than 0")
 
     @property
     def hash(self) -> defaultdict:
