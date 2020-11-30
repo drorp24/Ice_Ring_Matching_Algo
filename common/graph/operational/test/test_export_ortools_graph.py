4a6671b18057610e25179d3ba247969c7918d35f
meital issers <meitalg@gmail.com>
2020-11-30 17:08:20 +0200

    fix test

--- common/graph/operational/test/test_export_ortools_graph.py
+++ common/graph/operational/test/test_export_ortools_graph.py
@@ -1,11 +1,13 @@
 import unittest
+from datetime import date, time
 from random import Random
 
 from common.entities.delivery_request import DeliveryRequestDistribution
+from common.entities.drone_loading_dock import DroneLoadingDockDistribution
 from common.entities.package import PackageType
+from common.entities.temporal import DateTimeExtension
 from common.graph.operational.delivery_request_graph import OperationalGraph, OperationalEdge, OperationalNode, \
     OperationalEdgeAttributes
-from common.entities.drone_loading_dock import DroneLoadingDockDistribution
 from common.graph.operational.export_ortools_graph import OrtoolsGraphExporter
 
 
@@ -32,16 +34,18 @@ class BasicOrtoolsExporterTestCases(unittest.TestCase):
         return edges
 
     def test_export_time_window(self):
-        time_windows = self.graph_exporter.export_time_windows(self.operational_graph)
+        zero_time = DateTimeExtension(dt_date=date(2021, 1, 1),dt_time=time(0, 0, 0))
+        time_windows = self.graph_exporter.export_time_windows(self.operational_graph,zero_time)
+
         self.assertEqual(len(self.operational_graph.nodes), len(time_windows))
-        self.assertEqual(time_windows[0], self.dr_dataset_random[0].time_window.get_time_stamp())
-        self.assertEqual(time_windows[1], self.dr_dataset_random[1].time_window.get_time_stamp())
-        self.assertEqual(time_windows[4], self.dr_dataset_random[4].time_window.get_time_stamp())
-        self.assertEqual(time_windows[6], self.dr_dataset_random[6].time_window.get_time_stamp())
-        self.assertEqual(time_windows[9], self.dr_dataset_random[9].time_window.get_time_stamp())
-        self.assertEqual(time_windows[10], self.dld_dataset_random[0].time_window.get_time_stamp())
-        self.assertEqual(time_windows[11], self.dld_dataset_random[1].time_window.get_time_stamp())
-        self.assertEqual(time_windows[12], self.dld_dataset_random[2].time_window.get_time_stamp())
+        self.assertEqual(time_windows[0], self.dr_dataset_random[0].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[1], self.dr_dataset_random[1].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[4], self.dr_dataset_random[4].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[6], self.dr_dataset_random[6].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[9], self.dr_dataset_random[9].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[10], self.dld_dataset_random[0].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[11], self.dld_dataset_random[1].time_window.get_relative_time_in_min(zero_time))
+        self.assertEqual(time_windows[12], self.dld_dataset_random[2].time_window.get_relative_time_in_min(zero_time))
 
     def test_export_priorities(self):
         priorities = self.graph_exporter.export_priorities(self.operational_graph)
@@ -71,10 +75,9 @@ class BasicOrtoolsExporterTestCases(unittest.TestCase):
     def test_drone_loading_docks_indices(self):
         drd_indices = self.graph_exporter.export_basis_nodes_indices(self.operational_graph)
         self.assertEqual(len(self.dld_dataset_random), len(drd_indices))
-        self.assertEqual(drd_indices, list(range(10,13)))
+        self.assertEqual(drd_indices, list(range(10, 13)))
 
     def test_package_type_demands(self):
-        large_package_demands = self.graph_exporter.export_package_type_demands(self.operational_graph, PackageType.LARGE)
-        self.assertEqual(len(large_package_demands), len(self.dr_dataset_random))
-
-
+        large_package_demands = self.graph_exporter.export_package_type_demands(self.operational_graph,
+                                                                                PackageType.LARGE)
+        self.assertEqual(len(large_package_demands), len(self.dr_dataset_random) + len(self.dld_dataset_random))
