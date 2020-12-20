f4fe09f243957f00c9fcccb0b057a069e5dcc899
Roee Ben Sushan <roeebs@visionmap.com>
2020-12-20 16:34:47 +0200

    Fix depot time window

--- end_to_end/tests/test_assignment_presentation.py
+++ end_to_end/tests/test_assignment_presentation.py
@@ -37,6 +37,13 @@ north_lat = 32.19276
 
 ZERO_TIME = DateTimeExtension(dt_date=date(2021, 1, 1), dt_time=time(0, 0, 0))
 
+def create_standad_full_day_test_time():
+    default_start = ZERO_TIME
+    default_time_delta_distrib = TimeDeltaDistribution([TimeDeltaExtension(timedelta(hours=23, minutes=59))])
+    default_dt_options = [default_start]
+    return TimeWindowDistribution(DateTimeDistribution(default_dt_options), default_time_delta_distrib)
+
+
 
 def _create_delivery_request_distribution():
     package_distribution = create_single_package_distribution()
@@ -90,7 +97,8 @@ class BasicMinimumEnd2EndPresentation(unittest.TestCase):
                                                                          UniformPointInBboxDistribution(west_lon,
                                                                                                         east_lon,
                                                                                                         south_lat,
-                                                                                                        north_lat))))
+                                                                                                        north_lat)),
+                                         time_window_distributions=create_standad_full_day_test_time()))
         cls.matcher_config = Path("end_to_end/tests/jsons/test_matcher_config.json")
 
     def test_small_scenario(self):
