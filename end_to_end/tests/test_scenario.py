d937c51a464c421ef5bfceac51f563cbba54a9a7
Roee Ben Sushan <roeebs@visionmap.com>
2020-12-29 10:48:24 +0200

    Add writing scenario object to json file

--- end_to_end/tests/test_scenario.py
+++ end_to_end/tests/test_scenario.py
@@ -1,4 +1,5 @@
 import unittest
+from pathlib import Path
 from random import Random
 
 from common.entities.base_entities.delivery_request import DeliveryRequest
@@ -15,3 +16,11 @@ class BasicScenarioTests(unittest.TestCase):
         self.assertEqual(len(self.scenario.delivery_requests), 10)
         self.assertEqual(len(self.scenario.drone_loading_docks), 1)
 
+    def test_writing_scenario(self):
+        test_json_file_name = Path('end_to_end/tests/jsons/test_writing_scenario.json')
+        self.scenario.to_json(test_json_file_name)
+        loaded_scenario_dict = Scenario.json_to_dict(test_json_file_name)
+        loaded_scenario = self.scenario.dict_to_obj(loaded_scenario_dict)
+        self.assertEqual(self.scenario.zero_time, loaded_scenario.zero_time)
+        self.assertEqual(self.scenario.drone_loading_docks, loaded_scenario.drone_loading_docks)
+        self.assertEqual(self.scenario.delivery_requests, loaded_scenario.delivery_requests)
