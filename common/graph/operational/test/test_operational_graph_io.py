ae364d873bcd2667edc91c7dabf503a44938fa6e
gilbaz <gil.elbaz13@gmail.com>
2021-01-03 14:03:43 +0200

    make hierarchial distribution amount

--- common/graph/operational/test/test_operational_graph_io.py
+++ common/graph/operational/test/test_operational_graph_io.py
@@ -3,7 +3,7 @@ from random import Random
 
 from common.entities.base_entities.delivery_request import DeliveryRequest
 from common.entities.base_entities.entity_distribution.delivery_request_distribution import DeliveryRequestDistribution
-from common.graph.operational.operational_graph import OperationalNode, NonLocalizableNodeException
+from common.graph.operational.operational_graph import OperationalNode
 
 
 class BasicGraphNodeTestCases(unittest.TestCase):
@@ -12,9 +12,12 @@ class BasicGraphNodeTestCases(unittest.TestCase):
     def setUpClass(cls):
         cls.dr_dataset_random = DeliveryRequestDistribution().choose_rand(random=Random(100),
                                                                           amount={DeliveryRequest: 10})
+
         cls.example_node = OperationalNode(internal_node=cls.dr_dataset_random[0])
 
     def test_localizable_node_exception(self):
         op_node_dict = self.example_node.__dict__()
         self.assertTrue('internal_node' in op_node_dict.keys())
         self.assertTrue(op_node_dict['__class__'] is 'OperationalNode')
+        self.assertTrue(op_node_dict['internal_node']['__class__'] is 'DeliveryRequest')
+        self.assertTrue(DeliveryRequest.dict_to_obj(op_node_dict['internal_node']), self.example_node.internal_node)
