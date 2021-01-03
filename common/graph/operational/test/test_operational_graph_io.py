e3f44993689be4148f18991614e19b70bb208b79
gilbaz <gil.elbaz13@gmail.com>
2021-01-03 14:30:33 +0200

    make hierarchial distribution amount

--- common/graph/operational/test/test_operational_graph_io.py
+++ common/graph/operational/test/test_operational_graph_io.py
@@ -2,7 +2,10 @@ import unittest
 from random import Random
 
 from common.entities.base_entities.delivery_request import DeliveryRequest
+from common.entities.base_entities.drone_loading_dock import DroneLoadingDock
 from common.entities.base_entities.entity_distribution.delivery_request_distribution import DeliveryRequestDistribution
+from common.entities.base_entities.entity_distribution.drone_loading_dock_distribution import \
+    DroneLoadingDockDistribution
 from common.graph.operational.operational_graph import OperationalNode
 
 
@@ -11,13 +14,24 @@ class BasicGraphNodeTestCases(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dr_dataset_random = DeliveryRequestDistribution().choose_rand(random=Random(100),
-                                                                          amount={DeliveryRequest: 10})
+                                                                          amount={DeliveryRequest: 3})
+        cls.example_node_delivery_request = OperationalNode(internal_node=cls.dr_dataset_random[0])
 
-        cls.example_node = OperationalNode(internal_node=cls.dr_dataset_random[0])
+        cls.dld_dataset_random = DroneLoadingDockDistribution().choose_rand(random=Random(100), amount=2)
+        cls.example_node_drone_loading_dock = OperationalNode(internal_node=cls.dld_dataset_random[0])
 
-    def test_localizable_node_exception(self):
-        op_node_dict = self.example_node.__dict__()
+    def test_delivery_request_operational_node_to_dict(self):
+        op_node_dict = self.example_node_delivery_request.__dict__()
         self.assertTrue('internal_node' in op_node_dict.keys())
         self.assertTrue(op_node_dict['__class__'] is 'OperationalNode')
         self.assertTrue(op_node_dict['internal_node']['__class__'] is 'DeliveryRequest')
-        self.assertTrue(DeliveryRequest.dict_to_obj(op_node_dict['internal_node']), self.example_node.internal_node)
+        self.assertTrue(DeliveryRequest.dict_to_obj(op_node_dict['internal_node']),
+                        self.example_node_delivery_request.internal_node)
+
+    def test_drone_loading_dock_operational_node_to_dict(self):
+        op_node_dict = self.example_node_drone_loading_dock.__dict__()
+        self.assertTrue('internal_node' in op_node_dict.keys())
+        self.assertTrue(op_node_dict['__class__'] is 'OperationalNode')
+        self.assertTrue(op_node_dict['internal_node']['__class__'] is 'DroneLoadingDock')
+        self.assertTrue(DroneLoadingDock.dict_to_obj(op_node_dict['internal_node']),
+                        self.example_node_drone_loading_dock.internal_node)
