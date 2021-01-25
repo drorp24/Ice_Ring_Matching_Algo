95987781043f995f86acd1b73deb3db714b95386
Roee Ben Sushan <roeebs@visionmap.com>
2021-01-25 10:03:46 +0200

    Adding arrival envelope entities

--- drop_envelope/envelope_collections.py
+++ drop_envelope/envelope_collections.py
@@ -27,7 +27,13 @@ class PotentialEnvelopeCollection:
         raise NotImplementedError
 
 
-
 def get_distantest_shapeable(external_point: Point2D, potential_envelope: List[ShapeableCollection]) -> Shapeable:
-    #TODO: Not finished yet
-    for i, shapeables in enumerate(potential_envelope):
+    distantest_shapeables = []
+    distances = []
+    for collection in potential_envelope:
+        shapeable_distances = list(map(lambda shapeable:
+                                       external_point.calc_distance_to_point(shapeable.calc_location()),
+                                       collection.shapeabls()))
+        distances.append(max(shapeable_distances))
+        distantest_shapeables.append(collection.shapeabls()[shapeable_distances.index(max(shapeable_distances))])
+    return distantest_shapeables[distances.index(max(distances))]
