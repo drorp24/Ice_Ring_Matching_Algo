ef683572723781748f5c54bc6b54e03c42e7b7ea
meital issers <meitalg@gmail.com>
2021-02-08 11:13:15 +0200

    renaming

--- end_to_end/minimum_end_to_end.py
+++ end_to_end/minimum_end_to_end.py
@@ -18,14 +18,14 @@ def create_fully_connected_graph_model(supplier_category: SupplierCategory,
 def create_clustered_delivery_requests_graph_model(supplier_category: SupplierCategory,
                                                    edge_cost_factor: float = 1.0,
                                                    edge_travel_time_factor: float = 1.0,
-                                                   max_clusters: int = 1
+                                                   max_clusters_per_zone: int = 1
                                                    ) -> OperationalGraph:
     return create_clustered_delivery_requests_graph(delivery_requests=supplier_category.delivery_requests,
                                                     drone_loading_docks=supplier_category.drone_loading_docks,
                                                     zones=supplier_category.zones,
                                                     edge_cost_factor=edge_cost_factor,
                                                     edge_travel_time_factor=edge_travel_time_factor,
-                                                    max_clusters=max_clusters)
+                                                    max_clusters=max_clusters_per_zone)
 
 
 def calc_assignment(matcher_input: MatcherInput) -> DroneDeliveryBoard:
