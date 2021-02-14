a807bf201e40c6f0f885fc6709254fa19e9dc6cc
zivna <zivna@rafael.co.il>
2021-02-14 13:27:06 +0200

    add property to entityID

--- common/entities/base_entities/entity_id.py
+++ common/entities/base_entities/entity_id.py
@@ -1,4 +1,5 @@
 from uuid import UUID
+from typing import Union
 
 from common.entities.base_entities.base_entity import JsonableBaseEntity
 from common.utils import uuid_utils
@@ -6,18 +7,23 @@ from common.utils import uuid_utils
 
 class EntityID(JsonableBaseEntity):
 
-    def __init__(self, uuid: UUID):
+    def __init__(self, uuid: Union[UUID,str]):
         self._uuid = uuid
+        self._internal_type = uuid.__class__.__name__
 
     @property
     def uuid(self):
         return self._uuid
 
+    @property
+    def internal_type(self):
+        return self._internal_type
+
     @classmethod
     def dict_to_obj(cls, dict_input):
-        if dict_input.__class__ == str:
+        if dict_input['internal_type'] == str:
             return EntityID(dict_input)
-        if dict_input['__class__'] and dict_input['__class__'] == cls.__name__:
+        if dict_input['internal_type'] =='UUID':
             return EntityID(uuid_utils.convert_str_to_uuid(dict_input['uuid']))
 
     def __hash__(self):
--- common/utils/uuid_utils.py
+++ common/utils/uuid_utils.py
@@ -2,11 +2,7 @@ from uuid import UUID
 
 
 def convert_str_to_uuid(uuid_str: str) -> UUID:
-    try:
-        uuid = UUID(hex=uuid_str)
-    except ValueError:
-        # uuid = UUID(bytes=uuid_str.encode())
-        uuid=uuid_str
+    uuid = UUID(hex=uuid_str)
     return uuid
 
 
