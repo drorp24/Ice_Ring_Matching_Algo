dfe42ff7ecf2802ea2dbf057e42559bff313e222
zivna <zivna@rafael.co.il>
2021-03-03 08:43:59 +0200

    add file name to utils export data

--- common/utils/sample_data_utils.py
+++ common/utils/sample_data_utils.py
@@ -12,15 +12,20 @@ class ExportSamplesStatus(enum.Enum):
     ENABLED = 2
 
 
-def export_samples(object, run_id, enable_export=ExportSamplesStatus.DISABLED.name):
+def resolve_file_name_from_object_class(obj):
+        if isinstance(obj, list):
+            file_name = obj[0].__class__.__name__ + 's'
+        else:
+            file_name = obj.__class__.__name__
+        return file_name
 
-    if enable_export == ExportSamplesStatus.ENABLED.name :
-        if object:
-            if isinstance(object, list):
-                file_name = object[0].__class__.__name__ + 's'
-            else:
-                file_name = object.__class__.__name__
-            dir = 'exported_jsons/' + run_id
-            Path(dir).mkdir(parents=True, exist_ok=True)
-            with open(dir + '/' + file_name + ".json", "w") as write_file:
-                json.dump(object, write_file, default=to_dict)
+
+
+def export_samples(obj, run_id, enable_export=ExportSamplesStatus.DISABLED.name,file_name=None):
+    if enable_export == ExportSamplesStatus.ENABLED.name:
+        if not file_name:
+            file_name = resolve_file_name_from_object_class(obj)
+        dir = 'exported_jsons/' + run_id
+        Path(dir).mkdir(parents=True, exist_ok=True)
+        with open(dir + '/' + file_name + ".json", "w") as write_file:
+            json.dump(obj, write_file, default=to_dict)
