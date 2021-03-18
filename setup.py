4da2cd152c372fada1370111b81023324933030f
ITAYM1 <ITAYM1@rnd-hub.com>
2021-03-18 16:07:32 +0200

    * adding packaging configuration. * supporting: building everything from source       python setup.py install installing with pip       pip install -e .       pip install git+https://rnd-hub.visualstudio.com/Ice_Ring/_git/Ice_Ring_Matching_Algo

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+from setuptools import setup, find_packages
+
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_file = 'requirements.txt'
+full_path = '/'.join([lib_folder, requirement_file])
+
+install_requires = []
+if os.path.isfile(full_path):
+    with open(full_path) as f:
+        install_requires = f.read().splitlines()
+
+setup(name='MatchingAlgorithms',
+      version='0.0.1',
+      description='',
+      install_requires=install_requires,
+      build_requires=['Cython'],
+      author='',
+      packages=find_packages())
