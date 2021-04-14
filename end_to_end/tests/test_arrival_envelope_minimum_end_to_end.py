df7669038e4b10dc5aa0e3a1031294c3d10c554f
Ronen Ben-Hayon <ronenbh1@gmail.com>
2021-04-15 00:18:25 +0300

    Fixed BasicArrivalEnvelopeMinimumEnd2End

--- end_to_end/tests/test_arrival_envelope_minimum_end_to_end.py
+++ end_to_end/tests/test_arrival_envelope_minimum_end_to_end.py
@@ -1,22 +1,16 @@
 import os
 import unittest
-from datetime import timedelta
 from pathlib import Path
 
 from common.entities.base_entities.drone import DroneType, PackageConfiguration
 from common.entities.base_entities.drone_formation import DroneFormationType
-from common.entities.base_entities.drone_loading_dock import DroneLoadingDock
-from common.entities.base_entities.drone_loading_station import DroneLoadingStation
-from common.entities.base_entities.entity_id import EntityID
 from common.entities.base_entities.fleet.empty_drone_delivery_board_generation import generate_empty_delivery_board
 from common.entities.base_entities.fleet.fleet_property_sets import DroneSetProperties, DroneFormationTypePolicy, \
     PackageConfigurationPolicy
 from common.entities.base_entities.package import PackageType
-from common.entities.base_entities.temporal import TimeWindowExtension, TimeDeltaExtension
-from common.entities.base_entities.tests.test_drone_delivery import ZERO_TIME
-from end_to_end.arrival_envelope_minimum_end_to_end import create_time_overlapping_dependent_graph_model, calc_assignment
+from end_to_end.arrival_envelope_minimum_end_to_end import create_time_overlapping_dependent_graph_model, \
+    calc_assignment
 from end_to_end.supplier_category import SupplierCategory
-from geometry.geo_factory import create_point_2d
 from matching.matcher_config import MatcherConfig
 from matching.matcher_input import MatcherInput
 
@@ -27,30 +21,25 @@ class BasicArrivalEnvelopeMinimumEnd2End(unittest.TestCase):
     def setUpClass(cls):
         cls.supplier_category = SupplierCategory.dict_to_obj(SupplierCategory.json_to_dict(
             Path('end_to_end/tests/jsons/test_supplier_category.json')))
-        cls.empty_drone_delivery_board = \
-            generate_empty_delivery_board(drone_set_properties=[BasicArrivalEnvelopeMinimumEnd2End._create_simple_drone_set_properties()],
-                                          max_route_time_entire_board=400,
-                                          velocity_entire_board=10)
         cls.matcher_config = MatcherConfig.dict_to_obj(
             MatcherConfig.json_to_dict(Path('end_to_end/tests/jsons/test_min_e2e_config.json')))
+        cls.empty_drone_delivery_board = \
+            generate_empty_delivery_board(
+                drone_set_properties=[BasicArrivalEnvelopeMinimumEnd2End._create_simple_drone_set_properties(
+                    cls.supplier_category)],
+                max_route_time_entire_board=cls.matcher_config.constraints.travel_time.max_route_time,
+                velocity_entire_board=10)
 
-    @classmethod
-    def _create_simple_drone_set_properties(cls):
-        loading_dock = DroneLoadingDock(EntityID.generate_uuid(),
-                                        DroneLoadingStation(EntityID.generate_uuid(), create_point_2d(0, 0)),
-                                        DroneType.drone_type_1,
-                                        TimeWindowExtension(
-                                            since=ZERO_TIME,
-                                            until=ZERO_TIME.add_time_delta(
-                                                TimeDeltaExtension(timedelta(hours=5)))))
+    @staticmethod
+    def _create_simple_drone_set_properties(supplier_category: SupplierCategory):
         return DroneSetProperties(drone_type=DroneType.drone_type_1,
                                   drone_formation_policy=DroneFormationTypePolicy(
                                       {DroneFormationType.PAIR: 1.0, DroneFormationType.QUAD: 0.0}),
                                   package_configuration_policy=PackageConfigurationPolicy(
                                       {PackageConfiguration.LARGE_X2: 0.6, PackageConfiguration.MEDIUM_X4: 0.2,
                                        PackageConfiguration.SMALL_X8: 0.2, PackageConfiguration.TINY_X16: 0.0}),
-                                  start_loading_dock=loading_dock,
-                                  end_loading_dock=loading_dock,
+                                  start_loading_dock=supplier_category.drone_loading_docks[0],
+                                  end_loading_dock=supplier_category.drone_loading_docks[0],
                                   drone_amount=30)
 
     def test_create_graph_model(self):
