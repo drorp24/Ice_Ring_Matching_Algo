c19a90a237b65abd7c2243f0aaea65586d869366
GUYKL <guykl@rafael.co.il>
2021-04-21 15:17:38 +0300

    Merge with master

--- matching/ortools/ortools_priority_evaluator.py
+++ matching/ortools/ortools_priority_evaluator.py
@@ -13,31 +13,7 @@ class ORToolsPriorityEvaluator:
         self._index_manager = index_manager
         self._matcher_input = matcher_input
         self._reloader = reloader
-        # self._reloading_virtual_depos_indices = reloading_virtual_depos_indices
-        # self._num_of_nodes = len(self._matcher_input.graph.nodes) + len(self._reloading_virtual_depos_indices)
-        # self._priorities = {}
         self.priority_evaluator = self.create_priority_evaluator()
-    #
-    # def create_priority_evaluator(self):
-    #
-    #     def priority(_from_node):
-    #         if _from_node in self._reloading_virtual_depos_indices:
-    #             _from_node = self._graph_exporter.export_basis_nodes_indices(self._matcher_input.graph)[0]
-    #         _priority = self._graph_exporter.export_priorities(self._matcher_input.graph)[_from_node] * \
-    #             self._matcher_input.config.constraints.priority.priority_cost_coefficient
-    #         return _priority
-    #
-    #     _priorities = {}
-    #     for from_node in range(self._num_of_nodes):
-    #         _priorities[from_node] = int(priority(from_node))
-    #
-    #     @lru_cache()
-    #     def priority_evaluator(_from_index):
-    #         return _priorities[self._index_manager.index_to_node(_from_index)]
-    #
-    #     return priority_evaluator
-
-
 
     def create_priority_evaluator(self):
 
@@ -45,10 +21,10 @@ class ORToolsPriorityEvaluator:
             if _from_node in self._reloader.reloading_virtual_depos_indices:
                 vehicle_of_node = self._reloader.get_reloading_depot_vehicle(_from_node)
                 dock = self._matcher_input.delivering_drones_board.delivering_drones_list[
-                                                                                    vehicle_of_node].start_loading_dock
+                    vehicle_of_node].start_loading_dock
                 _from_node = self._graph_exporter.get_node_graph_index(self._matcher_input.graph, dock)
             _priority = self._graph_exporter.export_priorities(self._matcher_input.graph)[_from_node] * \
-                self._matcher_input.config.constraints.priority.priority_cost_coefficient
+                        self._matcher_input.config.constraints.priority.priority_cost_coefficient
             return _priority
 
         _priorities = {}
