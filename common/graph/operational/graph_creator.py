fe7196943fb67f8f509fa568a74b9baf237791d7
meital issers <meitalg@gmail.com>
2021-04-22 11:12:55 +0300

    Build a graph that depends on the zones

--- common/graph/operational/graph_creator.py
+++ common/graph/operational/graph_creator.py
@@ -9,7 +9,7 @@ from common.entities.base_entities.zone import Zone
 from common.graph.operational.graph_utils import sort_delivery_requests_by_zone, split_delivery_requests_into_clusters, \
     get_delivery_requests_from_graph, has_overlapping_time_window, calc_travel_time_in_min, calc_cost, \
     calc_under_distance, filter_nodes_with_at_least_one_identical_package_type, \
-    filter_nodes_with_time_overlapping
+    filter_nodes_with_time_overlapping, has_at_least_one_identical_package_type
 from common.graph.operational.operational_graph import OperationalGraph, OperationalEdge, OperationalEdgeAttribs, \
     OperationalNode
 
@@ -38,6 +38,24 @@ def create_clustered_delivery_requests_graph(delivery_requests: [DeliveryRequest
     return graph
 
 
+def create_package_time_zones_dependent_graph_model(delivery_requests: [DeliveryRequest],
+                                                    drone_loading_docks: [DroneLoadingDock],
+                                                    zones: [Zone],
+                                                    edge_cost_factor: float = 1.0,
+                                                    edge_travel_time_factor: float = 1.0
+                                                    ) -> OperationalGraph:
+    delivery_requests_by_zone = sort_delivery_requests_by_zone(delivery_requests, zones)
+
+    graph = OperationalGraph()
+    list(map(add_locally_connected_dr_graph_packages_time_dependent, repeat(graph), delivery_requests_by_zone.values(),
+             repeat(edge_cost_factor),
+             repeat(edge_travel_time_factor),
+             repeat(math.inf)))
+    add_fully_connected_loading_docks(graph, drone_loading_docks, edge_cost_factor, edge_travel_time_factor)
+
+    return graph
+
+
 def add_locally_connected_dr_graph(graph, dr_connection_options: [DeliveryRequest],
                                    edge_cost_factor: float = 1.0,
                                    edge_travel_time_factor: float = 1.0,
@@ -49,6 +67,27 @@ def add_locally_connected_dr_graph(graph, dr_connection_options: [DeliveryReques
         end_dr_options = calc_under_distance(dr_connection_options, start_dr, max_distance_to_connect_km)
         for end_dr in end_dr_options:
             if has_overlapping_time_window(start_dr, end_dr):
+                cost = calc_cost(start_dr, end_dr, edge_cost_factor)
+                travel_time = calc_travel_time_in_min(start_dr, end_dr, edge_travel_time_factor)
+                edges.append(OperationalEdge(OperationalNode(start_dr),
+                                             OperationalNode(end_dr),
+                                             OperationalEdgeAttribs(cost, travel_time)))
+    graph.add_operational_edges(edges)
+
+
+def add_locally_connected_dr_graph_packages_time_dependent(graph, dr_connection_options: [DeliveryRequest],
+                                                           edge_cost_factor: float = 1.0,
+                                                           edge_travel_time_factor: float = 1.0,
+                                                           max_distance_to_connect_km=math.inf,
+                                                           delivery_option_index: int = 0
+                                                           ):
+    edges = []
+    graph.add_delivery_requests(dr_connection_options)
+    for start_dr in dr_connection_options:
+        end_dr_options = calc_under_distance(dr_connection_options, start_dr, max_distance_to_connect_km)
+        for end_dr in end_dr_options:
+            if has_overlapping_time_window(start_dr, end_dr) and has_at_least_one_identical_package_type(
+                    start_dr.delivery_options[delivery_option_index], end_dr.delivery_options[delivery_option_index]):
                 cost = calc_cost(start_dr, end_dr, edge_cost_factor)
                 travel_time = calc_travel_time_in_min(start_dr, end_dr, edge_travel_time_factor)
                 edges.append(OperationalEdge(OperationalNode(start_dr),
@@ -104,10 +143,10 @@ def build_package_dependent_connected_graph(graph: OperationalGraph,
         graph.add_operational_edges(edges)
 
 
-def build_package_and_time_dependent_connected_graph(graph: OperationalGraph,
-                                                     edge_cost_factor: float = 1.0,
-                                                     edge_travel_time_factor: float = 1.0,
-                                                     delivery_option_index: int = 0):
+def build_package_time_dependent_connected_graph(graph: OperationalGraph,
+                                                 edge_cost_factor: float = 1.0,
+                                                 edge_travel_time_factor: float = 1.0,
+                                                 delivery_option_index: int = 0):
     for selected_node_index, selected_node in enumerate(graph.nodes):
         destinations_by_packages = filter_nodes_with_at_least_one_identical_package_type(selected_node=selected_node,
                                                                                          optional_nodes=graph.nodes[
