441a9a3dda9e68ba42ce2664c83be4505bbd17bd
zivna <zivna@rafael.co.il>
2021-04-28 15:05:26 +0300

    update duplication of properties

--- common/entities/base_entities/package.py
+++ common/entities/base_entities/package.py
@@ -33,11 +33,9 @@ class Package(JsonableBaseEntity):
 
     def __init__(self, weight: float, minimal_radius_meters: float, maximal_radius_meters: float):
         self._weight = weight
-        self._minimal_radius_meters = minimal_radius_meters
-        self._maximal_radius_meters = maximal_radius_meters
         self.__potential_drop_envelope = PotentialDropEnvelope(
-            minimal_radius_meters=self._minimal_radius_meters,
-            maximal_radius_meters=self._maximal_radius_meters)
+            minimal_radius_meters=minimal_radius_meters,
+            maximal_radius_meters=maximal_radius_meters)
 
     @property
     def weight(self) -> float:
@@ -45,18 +43,17 @@ class Package(JsonableBaseEntity):
 
     @property
     def minimal_radius_meters(self) -> float:
-        return self._minimal_radius_meters
+        return self.__potential_drop_envelope.minimal_radius_meters
 
     @property
     def maximal_radius_meters(self) -> float:
-        return self._maximal_radius_meters
+        return self.__potential_drop_envelope.maximal_radius_meters
 
     def calc_potential_drop_envelope(self) -> PotentialDropEnvelope:
         return self.__potential_drop_envelope
 
     def __str__(self):
-        return f'package of weight: {str(self._weight)},rmax of: {self.maximal_radius_meters},' \
-               f'rmin of: {self.minimal_radius_meters} '
+        return f'package of weight: {str(self._weight)},potential drop env of: {str(self.__potential_drop_envelope)}'
 
 
 class PackageType(Enum):
@@ -74,13 +71,14 @@ class PackageType(Enum):
     def calc_weight(self):
         return self.value.weight
 
-    def get_rmin_rmax(self):
-        return self.value.minimal_radius_meters, self.value.maximal_radius_meters
+    def get_rmin(self):
+        return PackageType.TINY.value.rmin
+
+    def get_rmax(self):
+        return PackageType.TINY.value.rmax
 
     def __dict__(self):
-        rmin, rmax = self.get_rmin_rmax()
-        return {'__enum__': str(self), 'minimal_radius_meters': rmin,
-                'maximal_radius_meters': rmax}
+        return {'__enum__': str(self)}
 
     def __repr__(self):
         return 'PackageType: ' + str(self.__dict__())
@@ -90,7 +88,7 @@ class PackageType(Enum):
 
     def __eq__(self, other: PackageType):
         return all([self.name == other.name, self.calc_weight() == other.calc_weight(),
-                    self.get_rmin_rmax() == other.get_rmin_rmax()])
+                    self.calc_potential_drop_envelope() == other.calc_potential_drop_envelope()])
 
     def __lt__(self, other: PackageType):
         return self.name < other.name
